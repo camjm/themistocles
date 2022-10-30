04fcc97772ec9c542dcd5d9d9ef7408c78607f52
Cam <2276730+camjm@users.noreply.github.com>
2022-10-30 19:22:53 +1300

    expose node stuff

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 var fs = require('fs');
 
-var package = fs.readFileSync('./package.json', 'utf8');
-alert(package);
+try {
+    const platform = document.querySelector('#platform');
+    const version = document.querySelector('#version');
+    const package = document.querySelector('#package');
+    platform.textContent = process.platform;
+    version.textContent = process.version;
+    package.textContent = fs.readFileSync('./package.json', 'utf8');
+} catch (error) {
+    alert(error);
+}
--- main.js
+++ main.js
@@ -4,6 +4,11 @@ const { app, BrowserWindow } = require('electron');
 const createWindow = () => {
     // BrowserWindow creates and manages windows
     var win = new BrowserWindow({
+        webPreferences: {
+            // these are required to expose node stuff in the Renderer process
+            nodeIntegration: true,
+            contextIsolation: false
+        },
         width: 800,
         height: 600
     });
